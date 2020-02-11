f0c44dcd75d5a9bd649e94fb2b1001644408d224
ALEX <alexjonesy@hotmail.co.uk>
2020-02-11 14:09:26 +0000

    java thing

---
+++ RewriteCode.java
@@ -0,0 +1,12 @@
+// package import missing
+import java.util.Scanner;
+public class RewriteCode {
+	public static void main( String args[] ) {
+	    Scanner in = new Scanner( System.in );
+
+	    System.out.println( "Enter an integer between 0 and 9" );
+	    int num = in.nextInt();
+
+	    System.out.println( "You entered " + num );
+	}
+}
